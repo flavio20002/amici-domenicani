9d0e5edd44c016fdc39ac217401f3eb5725f288e
Flavio Barisi <flvb86@gmail.com>
2023-07-11 20:38:08 +0200

    Fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,6 +13,6 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-    expect(find.text('Amici Domenicani'), findsOneWidget);
+    expect(find.text('Articoli'), findsWidgets);
   });
 }
